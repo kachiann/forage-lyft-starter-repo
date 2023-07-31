3eecb4cf7a501b61aaed9f6da379e8f59c4464e0
Onyekachi (Osisiogu) Emenike <dotteanna62@gmail.com>
2023-07-31 14:33:27 +0200

    correct __init__

--- Tire/carrigan_tire.py
+++ Tire/carrigan_tire.py
@@ -1,7 +1,7 @@
 from Tire.Tire import Tire
 
 class CarriganTire(Tire):
-    def __int__(self, tire_array):
+    def __init__(self, tire_array):
         self.tire_array = tire_array
 
     def needs_service(self)-> bool:
